f985a58402226c3a1c0759f0b2eb4b2356ad5c41
Luke Blaney <git@lukeblaney.co.uk>
2019-04-13 22:29:33 +0100

    include letsencrypt certbot and a script to start setting up certs

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,15 @@ RUN rm /etc/nginx/conf.d/*
 RUN rm -rf /usr/share/nginx/html
 
 COPY conf /etc/nginx/conf.d
+
+RUN printf "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/backports.list
+RUN apt-get update
+RUN apt-get -t stretch-backports install certbot python-certbot-nginx -y
+
+COPY templates/https.conf /etc/nginx/https-template.conf
+COPY domain-list          /etc/nginx/domain-list
+COPY update-domains.sh    /usr/bin/update-domains.sh
+
+RUN chmod +x /usr/bin/update-domains.sh
+
+CMD /usr/bin/update-domains.sh
---
+++ update-domains.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+set -m
+
+if [ -z "$ADMINEMAIL" ]; then
+    echo "Need to set ADMINEMAIL (used for let's encrypt renewal emails)"
+    exit 1
+fi  
+
+# Start up nginx in the background
+nginx -g "daemon off;" &
+
+echo "Checking domain list"
+domainlist=$(cat /etc/nginx/domain-list)
+
+for DOMAIN in $domainlist;
+do
+	echo "Renewing domain: $DOMAIN"
+	certbot --staging --nginx -d $DOMAIN --agree-tos -m $ADMINEMAIL && \
+	
+	# TODO: replace domain name into template
+	cat /etc/nginx/https-template.conf >> /etc/nginx/conf.d/$DOMAIN.conf && \
+	service nginx reload || true
+done
+
+# Bring nginx to the foreground
+fg %1
